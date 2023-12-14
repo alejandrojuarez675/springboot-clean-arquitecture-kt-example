3c639ee9c949e2c38f76231f8d674cae68ce793e
alejandro juarez <ext_alejjuar@mercadolibre.com>
2023-12-14 17:13:33 -0300

    first commit

---
+++ src/main/kotlin/com/alejua/example/springbootcleanarquitecturektexample/SpringbootCleanArquitectureKtExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.alejua.example.springbootcleanarquitecturektexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootCleanArquitectureKtExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootCleanArquitectureKtExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/alejua/example/springbootcleanarquitecturektexample/SpringbootCleanArquitectureKtExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alejua.example.springbootcleanarquitecturektexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootCleanArquitectureKtExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
